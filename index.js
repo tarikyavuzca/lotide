0e77d97f2873bd94a6f2adbc36947768c11d5a7c
tarikyavuzca <tarikyavuzca@gmail.com>
2021-09-20 19:24:38 -0400

    index file to export all files inside of lotide folder

---
+++ index.js
@@ -0,0 +1,36 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPosition = require('./letterPosition');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPosition,
+  map,
+  takeUntil,
+  without,
+  assertArraysEqual,
+  assertEqual
+};
