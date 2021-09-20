a71d7703078a23a9fc5a370a825acbb7a009b74b
tarikyavuzca <tarikyavuzca@gmail.com>
2021-09-20 15:14:55 -0400

    test folder created and test cases are moved to this folder

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,13 @@
 // FUNCTION IMPLEMENTATION
+
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log("Assertation Passed: " + actual + " === " + expected);
+    console.log(`Assertation Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("Assertation Failed: " + actual + " !== " + expected);
+    console.log(`Assertation Failed: ${actual} !== ${expected}`);
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,18 +1,8 @@
+const assertEqual = require('./assertEqual');
+
 const head = function(arr) {
-  for(let i = 0; i < arr.length; i++) 
+  for (let i = 0; i < arr.length; i++)
     return arr[i];
-}
-
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`Assertation Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`Assertation Failed: ${actual} !== ${expected}`);
-  }
 };
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
